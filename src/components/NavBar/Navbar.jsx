eacd6f4665eab1817bd8f7919ddb444b0a49a387
dreedup <andredransom@gmail.com>
2023-09-15 23:46:13 -0400

    added Navbar"

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className='Nav'>The Journey
+    <div className="topLeft"></div>
+    <div className="topCenter"></div>
+    <div className="topRight"></div>
+    </div>
+  
+  )
+}
